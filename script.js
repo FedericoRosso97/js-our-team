e30cf4f3abf3289f0d1c420a698da09924940f77
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-29 16:21:57 +0200

    append all images in dom

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ teamMembers=[
    {
     nome:'Wayne bannet',
     ruolo:'founder & CEO',
-    foto:'<img src="img/wayne-barnett-founder-ceo.jpg">',
+    foto:"img/wayne-barnett-founder-ceo.jpg",
    },
    {
     nome:'Angela Caroll',
     ruolo:'Chief Editor',
-    foto:'<img src="img/angela-caroll-chief-editor.jpg">',
+    foto:"img/angela-caroll-chief-editor.jpg"
    },
    {
     nome:'Walter Gordon',
     ruolo:'Office Manager ',
-    foto:'<img src="img/walter-gordon-office-manager.jpg">',
+    foto:"img/walter-gordon-office-manager.jpg",
    },
    {
     nome:'Angela Lopez',
     ruolo:'Social Media',
-    foto:'<img src="img/angela-lopez-social-media-manager.jpg">',
+    foto:"img/angela-lopez-social-media-manager.jpg",
    },
    {
     nome:'Scott Estradat',
     ruolo:'Developer',
-    foto:'<img src="img/scott-estrada-developer.jpg">',
+    foto:"img/scott-estrada-developer.jpg",
    },
    {
     nome:'Barbara Ramo',
     ruolo:'Graphic Designer',
-    foto:'<img src="img/barbara-ramos-graphic-designer.jpg">',
+    foto:"img/barbara-ramos-graphic-designer.jpg",
    }
 ];
 
